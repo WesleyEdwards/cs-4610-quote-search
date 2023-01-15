7ff880bb3ed767bbfe2e639f05eceb28f740751f
WesleyEdwards <westheedwards@gmail.com>
2023-01-15 09:42:29 -0700

    Making submit a form

--- src/SearchBox.tsx
+++ src/SearchBox.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC, FormEventHandler, useState } from "react";
 
 interface SearchBoxProps {
   handleSubmit: (name: string | undefined) => void;
@@ -9,44 +9,50 @@ export const SearchBox: FC<SearchBoxProps> = (props) => {
   const [hover, setHover] = useState<boolean>(false);
   const [name, setName] = useState<string>();
 
-  const onSubmit = () => {
+  const onSubmit: FormEventHandler = (e) => {
+    e.preventDefault();
     handleSubmit(name);
   };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "center",
-        gap: "1rem",
-        maxWidth: "24rem",
-      }}
-    >
-      <input
-        type="text"
+    <form onSubmit={onSubmit}>
+      <div
         style={{
-          fontSize: "1.25rem",
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "1rem",
+          maxWidth: "24rem",
         }}
-        placeholder="Enter a name"
-        value={name ?? ""}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <button
-        onClick={onSubmit}
-        style={{
-          backgroundColor: hover ? "lightgray" : "white",
-          border: "1px solid black",
-          borderRadius: "0.5rem",
-          padding: "0.5rem",
-          cursor: "pointer",
-        }}
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
       >
-        Search
-      </button>
-    </div>
+        <input
+          type="text"
+          style={{
+            fontSize: "1rem",
+            padding: "0.5rem",
+            borderRadius: "0.5rem",
+            width: "100%",
+          }}
+          placeholder="Thomas Jefferson"
+          value={name ?? ""}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button
+          type="submit"
+          style={{
+            backgroundColor: hover ? "lightgray" : "white",
+            border: "1px solid black",
+            borderRadius: "0.5rem",
+            padding: "0.5rem",
+            cursor: "pointer",
+          }}
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+        >
+          Search
+        </button>
+      </div>
+    </form>
   );
 };
